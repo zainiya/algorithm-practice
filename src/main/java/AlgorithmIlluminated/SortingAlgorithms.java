3c51dc7d41c184648b2bedf70915e23d716ee50e
ZAINIYA MANJIYANI <37852523+zainiya@users.noreply.github.com>
2022-12-30 12:31:00 -0800

    insertion and bubble sort

--- src/main/java/AlgorithmIlluminated/SortingAlgorithms.java
+++ src/main/java/AlgorithmIlluminated/SortingAlgorithms.java
@@ -9,9 +9,14 @@ public class SortingAlgorithms {
 
     System.out.println("Sorting Algorithms:");
     System.out.println("sort using selection sort = " + Arrays.toString(selectionSort(toSort)));
+    System.out.println("sort using insertion sort = " + Arrays.toString(insertionSort(toSort)));
+    System.out.println("sort using bubble sort = " + Arrays.toString(bubbleSort(toSort)));
   }
 
   private int[] selectionSort(int[] toSort) {
+    if (toSort.length == 1) {
+      return toSort;
+    }
     for (int i = 0; i < toSort.length; i++) {
       int minIdx = i;
       for (int j = i + 1; j < toSort.length; j++) {
@@ -23,6 +28,39 @@ public class SortingAlgorithms {
       int temp = toSort[minIdx];
       toSort[minIdx] = toSort[i];
       toSort[i] = temp;
+    }
+    return toSort;
+  }
+
+  private int[] insertionSort(int[] toSort) {
+    if (toSort.length == 1) {
+      return toSort;
+    }
+    for (int i = 1; i < toSort.length; i++) {
+      int current = toSort[i];
+      int j = i - 1;
+
+      while (j > 0 && toSort[j] > current) {
+        toSort[j + 1] = toSort[j];
+        j = j - 1;
+      }
+      toSort[j + 1] = current;
+    }
+    return toSort;
+  }
+
+  private int[] bubbleSort(int[] toSort) {
+    if (toSort.length == 1) {
+      return toSort;
+    }
+    for (int i = 0; i < toSort.length; i++) {
+      for (int j = i + 1; j < toSort.length; j++) {
+        if (toSort[i] > toSort[j]) {
+          int temp = toSort[j];
+          toSort[j] = toSort[i];
+          toSort[i] = temp;
+        }
+      }
     }
     return toSort;
   }
